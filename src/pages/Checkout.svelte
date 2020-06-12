c4684d546331ae7e0a7d3cf2f81ac7571b5ac7a3
awais512 <raza.awais25@gmail.com>
2020-06-12 13:21:03 +0500

    Handling empty cart

--- src/pages/Checkout.svelte
+++ src/pages/Checkout.svelte
@@ -1,1 +1,21 @@
-<h1>Checkout Page</h1>
+<script>
+  import { onMount } from "svelte";
+  import { navigate, link } from "svelte-routing";
+  import user from "../stores/user";
+  import { cartTotal } from "../stores/cart";
+
+  onMount(() => {
+    if (!$user.jwt) {
+      navigate("/");
+    }
+  });
+</script>
+
+{#if $cartTotal > 0}
+  <h1>From</h1>
+{:else}
+  <div class="checkout-empty">
+    <h2>Your cart is empty</h2>
+    <a href="/products" use:link class="btn btn-primary">fill it</a>
+  </div>
+{/if}
