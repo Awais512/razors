e857cce11aee036ff0b8755db5d7a189bb003b15
awais512 <raza.awais25@gmail.com>
2020-06-12 18:13:57 +0500

    Stripe setup

--- src/pages/Checkout.svelte
+++ src/pages/Checkout.svelte
@@ -5,12 +5,29 @@
   import { cartTotal } from "../stores/cart";
 
   let name = "";
+  let cardElement;
+  let cardErrors;
+  let card;
+  let stripe;
+  let elements;
   $: isEmpty = !name;
 
   onMount(() => {
     if (!$user.jwt) {
       navigate("/");
+      return;
     }
+    stripe = Stripe("pk_test_YyrG6JgCeyztWadWEmTqz7Nt");
+    elements = stripe.elements();
+    card = elements.create("card");
+    card.mount(cardElement);
+    card.addEventListener("change", function(event) {
+      if (event.error) {
+        cardErrors.textContent = event.error.message;
+      } else {
+        cardErrors.textContent = "";
+      }
+    });
   });
 
   function handleSubmit() {
@@ -26,6 +43,21 @@
       <div class="form-control">
         <label for="name">Your name</label>
         <input type="text" id="name" bind:value={name} />
+      </div>
+
+      <div class="stripe-input">
+        <label for="card-element">Credit or debit card</label>
+        <p class="stripe-info">
+          Test using this credit card:
+          <span>4242 4242 4242 4242</span>
+          <br />
+          enter any 5 digits for zip code
+          <br />
+          enter any 3 digits for cvc
+        </p>
+
+        <div id="card-element" bind:this={cardElement} />
+        <div id="card-errors" bind:this={cardErrors} role="alert" />
       </div>
       {#if isEmpty}
         <p class="form-empty">Please fill out name field</p>
