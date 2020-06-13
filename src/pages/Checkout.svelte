45c5554895edbc9e53f52904e1a6d34da079d893
awais512 <raza.awais25@gmail.com>
2020-06-13 15:43:00 +0500

    App completed

--- src/pages/Checkout.svelte
+++ src/pages/Checkout.svelte
@@ -4,22 +4,23 @@
   import user from "../stores/user";
   import cart, { cartTotal } from "../stores/cart";
   import submitOrder from "../strapi/submitOrders";
-
+  import globalStore from "../stores/globalStore";
   let name = "";
+  // stripe vars
   let cardElement;
   let cardErrors;
   let card;
   let stripe;
   let elements;
-  $: isEmpty = !name;
-
+  // is Empty
+  $: isEmpty = !name || $globalStore.alert;
   onMount(() => {
     if (!$user.jwt) {
       navigate("/");
       return;
     }
     if ($cartTotal > 0) {
-      stripe = Stripe("pk_test_YyrG6JgCeyztWadWEmTqz7Nt");
+      stripe = Stripe("pk_test_u98wkoUC9x1zM28B8icCPH1y008TT9DFrS");
       elements = stripe.elements();
       card = elements.create("card");
       card.mount(cardElement);
@@ -32,23 +33,41 @@
       });
     }
   });
-
   async function handleSubmit() {
+    globalStore.toggleItem("alert", true, "submitting order... please wait!");
     let response = await stripe
       .createToken(card)
       .catch(error => console.log(error));
     const { token } = response;
     if (token) {
       const { id } = token;
-      let orders = await submitOrder({
+      // token.id
+      // submit the order
+      console.log(typeof $cartTotal);
+      let order = await submitOrder({
         name,
         total: $cartTotal,
         items: $cart,
         stripeTokenId: id,
         userToken: $user.jwt
       });
-      console.log(orders);
+      console.log(order);
+      if (order) {
+        globalStore.toggleItem("alert", true, "your order is complete!");
+        cart.set([]);
+        localStorage.setItem("cart", JSON.stringify([]));
+        navigate("/");
+        return;
+      } else {
+        globalStore.toggleItem(
+          "alert",
+          true,
+          "there was an error with your order. please try again!",
+          true
+        );
+      }
     } else {
+      console.log(response);
     }
   }
 </script>
@@ -57,29 +76,38 @@
   <section class="form">
     <h2 class="section-title">checkout</h2>
     <form class="checkout-form" on:submit|preventDefault={handleSubmit}>
-      <h3>order total: ${$cartTotal}</h3>
+      <h3>order total : ${$cartTotal}</h3>
+      <!-- single input -->
       <div class="form-control">
-        <label for="name">Your name</label>
+        <label for="name">your name</label>
         <input type="text" id="name" bind:value={name} />
       </div>
-
+      <!--end of  single input -->
+      <!-- stripe stuff -->
       <div class="stripe-input">
-        <label for="card-element">Credit or debit card</label>
+        <!-- info -->
+        <label for="card-element">Credit or Debit Card</label>
         <p class="stripe-info">
           Test using this credit card:
           <span>4242 4242 4242 4242</span>
           <br />
-          enter any 5 digits for zip code
+          enter any 5 digits for the zip code
           <br />
-          enter any 3 digits for cvc
+          enter any 3 digits for the CVC
         </p>
-
-        <div id="card-element" bind:this={cardElement} />
+        <div id="card-element" bind:this={cardElement}>
+          <!-- stripe -->
+        </div>
         <div id="card-errors" bind:this={cardErrors} role="alert" />
       </div>
+
+      <!-- end of stripe stuff -->
+
+      <!-- error message -->
       {#if isEmpty}
-        <p class="form-empty">Please fill out name field</p>
+        <p class="form-empty">please fill out name field</p>
       {/if}
+      <!-- submit button -->
       <button
         type="submit"
         class="btn btn-block btn-primary"
@@ -91,7 +119,7 @@
   </section>
 {:else}
   <div class="checkout-empty">
-    <h2>Your cart is empty</h2>
+    <h2>your cart is empty</h2>
     <a href="/products" use:link class="btn btn-primary">fill it</a>
   </div>
 {/if}
